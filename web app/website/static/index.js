fa77beba35fcc6f57a9b85d67b21b3b03a698d61
patmozdz <123921845+patmozdz@users.noreply.github.com>
2023-04-30 20:24:10 -0500

    Web app fully working at current version.
    Web app works. Can log in, create new user, save notes. Currently runs using SQLite. TODO: Create cloud database, transition to Django, add React frontend.

---
+++ web app/website/static/index.js
@@ -0,0 +1,8 @@
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId})
+    }).then((_rest) => {
+        window.location.href = "/";
+    })
+}
